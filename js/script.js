b635341e1c33fd7812fe23558375d312a5e1ac73
Arthur Maia Caneppele Valerio <arthurmaiav@gmail.com>
2019-04-22 22:05:10 -0300

    First version complete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var slideIndex = 1;
+
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -13,14 +14,29 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  var descs = document.getElementsByClassName("description");
+  
+  if (n > slides.length) {
+      slideIndex = 1; //Se passar do ultimo volta pro primeiro
+    }    
+
+  if (n < 1) {
+      slideIndex = slides.length; //Se voltar alem do primeiro vai pro ultimo
+    }
+
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
+
+  for (i = 0; i < descs.length; i++) {
+    descs[i].style.display = "none";  
+}
+
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+  descs[slideIndex-1].style.display = "block";
 }
